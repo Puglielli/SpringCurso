98505ca72851e1d8eaca38ed75b4a5069c62e196
puglielli <puglielli14@gmail.com>
2019-04-29 14:13:36 -0300

    Ajustes no endpoint /categoria/{id}

--- src/main/java/com/puglielli/cursomc/services/CategoriaService.java
+++ src/main/java/com/puglielli/cursomc/services/CategoriaService.java
@@ -1,4 +1,4 @@
-package com.puglielli.cursomc.services;
+	package com.puglielli.cursomc.services;
 
 import java.util.Optional;
 
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.puglielli.cursomc.domain.Categoria;
+import com.puglielli.cursomc.exceptions.ObjectNotFoundException;
 import com.puglielli.cursomc.repositories.CategoriaRepository;
 
 @Service
@@ -14,8 +15,9 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	}
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		}
 }
