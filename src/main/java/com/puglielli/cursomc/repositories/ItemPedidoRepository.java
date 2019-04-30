6d70657a501555514930488294b0504a36ae4c18
puglielli <puglielli14@gmail.com>
2019-04-30 11:38:14 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/puglielli/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,11 @@
+package com.puglielli.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.puglielli.cursomc.domain.ItemPedido;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
