f660eca57ea077c857756d7a5ecd69391f8c41a7
puglielli <puglielli14@gmail.com>
2019-04-30 15:52:47 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/puglielli/cursomc/services/CategoriaService.java
+++ src/main/java/com/puglielli/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.puglielli.cursomc.domain.Categoria;
+import com.puglielli.cursomc.dto.CategoriaDTO;
 import com.puglielli.cursomc.exceptions.DataIntegrityException;
 import com.puglielli.cursomc.exceptions.ObjectNotFoundException;
 import com.puglielli.cursomc.repositories.CategoriaRepository;
@@ -55,5 +56,9 @@ public class CategoriaService {
 	public 	Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
