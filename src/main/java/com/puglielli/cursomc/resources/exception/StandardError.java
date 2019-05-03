90b9062618aa2f70a92e2d01550bf62eb4280ae6
puglielli <puglielli14@gmail.com>
2019-05-03 09:40:37 -0300

    Personalisando o retorno da validacao

--- src/main/java/com/puglielli/cursomc/resources/exception/StandardError.java
+++ src/main/java/com/puglielli/cursomc/resources/exception/StandardError.java
@@ -4,41 +4,47 @@ import java.io.Serializable;
 
 public class StandardError implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer status;
-	private String msg;
-	private Long timeStamp;
+	private String message;
+	private Long timestamp;
 	
-	public StandardError(Integer status, String msg, Long timeStamp) {
+
+	public StandardError(Integer status, String message, Long timestamp) {
 		super();
 		this.status = status;
-		this.msg = msg;
-		this.timeStamp = timeStamp;
+		this.message = message;
+		this.timestamp = timestamp;
 	}
+
 
 	public Integer getStatus() {
 		return status;
 	}
 
+
 	public void setStatus(Integer status) {
 		this.status = status;
 	}
 
-	public String getMsg() {
-		return msg;
+
+	public String getMessage() {
+		return message;
 	}
 
-	public void setMsg(String msg) {
-		this.msg = msg;
+
+	public void setMessage(String message) {
+		this.message = message;
 	}
 
-	public Long getTimeStamp() {
-		return timeStamp;
+
+	public Long getTimestamp() {
+		return timestamp;
 	}
 
-	public void setTimeStamp(Long timeStamp) {
-		this.timeStamp = timeStamp;
+
+	public void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
 	}
-	
 	
 }
