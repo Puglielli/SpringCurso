751f02c0fbd65341529dccbe8afa20d5eed97861
puglielli <puglielli14@gmail.com>
2019-05-03 15:32:36 -0300

    Validacao customizada: email não repetido na atualizacao de Cliente

--- src/main/java/com/puglielli/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/puglielli/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.puglielli.cursomc.domain.Cliente;
+import com.puglielli.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO  implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
