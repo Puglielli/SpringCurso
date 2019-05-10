9dcd3b2c77ceb688a6725f9714795e086aeebed2
puglielli <puglielli14@gmail.com>
2019-05-10 09:17:43 -0300

    Adicionando senha a Cliente

--- src/main/java/com/puglielli/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/puglielli/cursomc/dto/ClienteNewDTO.java
@@ -23,7 +23,10 @@ public class ClienteNewDTO implements Serializable {
 	@NotEmpty(message="Preenchimento obrigatorio")
 	private String cpfOuCnpj;
 	
-	private Integer tipo;	
+	private Integer tipo;
+	
+	@NotEmpty(message="Preenchimento obrigatorio")
+	private String senha;
 
 	@NotEmpty(message="Preenchimento obrigatorio")
 	private String logradouro;
@@ -149,5 +152,13 @@ public class ClienteNewDTO implements Serializable {
 
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 }
