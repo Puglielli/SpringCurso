ec13b596aa90c04db3126bca40598d77a8f250c1
puglielli <puglielli14@gmail.com>
2019-05-14 14:19:27 -0300

    Enviando imagem via endpoint

--- src/main/java/com/puglielli/cursomc/CursomcApplication.java
+++ src/main/java/com/puglielli/cursomc/CursomcApplication.java
@@ -1,6 +1,5 @@
 package com.puglielli.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,13 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
 
-import com.puglielli.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
-	
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -22,7 +16,6 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\temp\\fotos\\Kimetsu.png");
 	}
 	
 	@Bean
