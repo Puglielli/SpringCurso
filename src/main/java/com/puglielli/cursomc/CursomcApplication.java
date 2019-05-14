a43c4aa4a445103ad0ed4fc2cc56f5aade599adb
puglielli <puglielli14@gmail.com>
2019-05-14 12:46:07 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/puglielli/cursomc/CursomcApplication.java
+++ src/main/java/com/puglielli/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.puglielli.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,8 +8,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
 
+import com.puglielli.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -16,6 +22,7 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("C:\\temp\\fotos\\Kimetsu.png");
 	}
 	
 	@Bean
