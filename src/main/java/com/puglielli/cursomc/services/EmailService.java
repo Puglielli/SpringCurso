c6e316578bd83e8fa9855b66166ccd3e0b4ad6c9
puglielli <puglielli14@gmail.com>
2019-05-14 09:58:29 -0300

    Esqueci a senha

--- src/main/java/com/puglielli/cursomc/services/EmailService.java
+++ src/main/java/com/puglielli/cursomc/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.puglielli.cursomc.domain.Cliente;
 import com.puglielli.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -15,4 +16,6 @@ public interface EmailService {
 	void sendHtmlEmail(MimeMessage msg);
 	
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
