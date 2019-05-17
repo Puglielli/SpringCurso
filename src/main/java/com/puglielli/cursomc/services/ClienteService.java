32e830fe76ae5eac6d7130fc03987e74abceca6c
puglielli <puglielli14@gmail.com>
2019-05-17 09:51:59 -0300

    Usando padrao de nomes para imagens

--- src/main/java/com/puglielli/cursomc/services/ClienteService.java
+++ src/main/java/com/puglielli/cursomc/services/ClienteService.java
@@ -1,10 +1,12 @@
 package com.puglielli.cursomc.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -42,6 +44,12 @@ public class ClienteService {
 	
 	@Autowired
 	private S3Service s3Service;
+	
+	@Autowired
+	private ImageService imageService;
+	
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
 	
 	
 	public Cliente find(Integer id) {
@@ -120,13 +128,9 @@ public class ClienteService {
 			throw new AuthorizationException("Acesso negado");
 		}
 		
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getId() + ".jpg";
 		
-		URI uri =  s3Service.uploadFile(multipartFile);
-		
-		Cliente cli = find(user.getId());
-		cli.setImageURL(uri.toString());
-		repo.save(cli);
-		
-		return uri;
+		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
 }
